28ddd63bd77868f4a727a3b26b8136c6c84b282c
JesseMiami100 <jessemetayer@gmail.com>
2022-11-23 02:08:06 +0000

    domainNameGenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["intelligent", "giant"];
+  let noun = ["jogger", "racoon"];
+  let end = [".com", ".net", ".edu"];
+  let element = document.getElementById("excuse");
+
+  let excuse = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < end.length; l++) {
+          excuse.push(pronoun[i] + adj[j] + noun[k] + end[l]);
+        }
+      }
+    }
+  }
+
+  element.innerHTML = excuse;
 };
